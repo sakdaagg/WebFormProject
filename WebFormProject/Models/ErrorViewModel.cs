a98ac55a0e7a9b2b87fc249b03a690675f358aa1
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2024-11-03 17:23:29 +0300

    Proje dosyası ekle.

---
+++ WebFormProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebFormProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
