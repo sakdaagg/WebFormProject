5121100180bf37d62e1a8d255b22ff345e92ac4d
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2024-11-04 14:22:19 +0300

    DBContext tanimi eklendi ve tanimlanan dbcontext in ilk migration isleminde gerekli konfigurasyonlarin alinarak otomatik triggerlanmasi icin DbContextFactory class tanimlandi.

---
+++ WebFormProject.DAL/DbContexts/WebFormDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebFromProject.Entities.Entities;
+
+namespace WebFormProject.DAL.DbContexts;
+
+public class WebFormDbContext : IdentityDbContext<AspNetUser, AspNetRole, Guid, AspNetUserClaim,
+                                               AspNetUserRole, AspNetUserLogin,
+                                               AspNetRoleClaim, AspNetUserToken>
+{
+    public WebFormDbContext(DbContextOptions<WebFormDbContext> options) : base(options)
+    {
+    }
+    public DbSet<AspNetUser> User { get; set; }
+    public DbSet<AspNetRole> Role { get; set; }
+    public DbSet<AspNetUserRole> UserRole { get; set; }
+    public DbSet<AspNetUserClaim> UserClaim { get; set; }
+    public DbSet<AspNetUserLogin> UserLogin { get; set; }
+    public DbSet<AspNetUserToken> UserToken { get; set; }
+    public DbSet<AspNetRoleClaim> RoleClaim { get; set; }
+}
---
+++ WebFormProject.DAL/DbContexts/WebFormDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace WebFormProject.DAL.DbContexts;
+
+public class WebFormDbContextFactory : IDesignTimeDbContextFactory<WebFormDbContext>
+{
+    public WebFormDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<WebFormDbContext>();
+
+        // appsettings.json dosyasından bağlantı dizesini okuma
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new WebFormDbContext(optionsBuilder.Options);
+    }
+}
