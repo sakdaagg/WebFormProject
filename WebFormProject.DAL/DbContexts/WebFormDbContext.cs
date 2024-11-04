83fb438642146aff2c88de223d86d86a2706efee
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2024-11-04 21:58:02 +0300

    WebFormDbContext icerisine configuration islemesi eklendi.

--- WebFormProject.DAL/DbContexts/WebFormDbContext.cs
+++ WebFormProject.DAL/DbContexts/WebFormDbContext.cs
@@ -11,6 +11,18 @@ public class WebFormDbContext : IdentityDbContext<AspNetUser, AspNetRole, Guid, 
     public WebFormDbContext(DbContextOptions<WebFormDbContext> options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        foreach (var configuration in this.GetType().Assembly.GetTypes()
+            .Where(t => t.GetInterfaces().Contains(typeof(IEntityTypeConfiguration<>))))
+        {
+            dynamic configInstance = Activator.CreateInstance(configuration);
+            modelBuilder.ApplyConfiguration(configInstance);
+        }
+    }
     public DbSet<AspNetUser> User { get; set; }
     public DbSet<AspNetRole> Role { get; set; }
     public DbSet<AspNetUserRole> UserRole { get; set; }
@@ -18,4 +30,5 @@ public class WebFormDbContext : IdentityDbContext<AspNetUser, AspNetRole, Guid, 
     public DbSet<AspNetUserLogin> UserLogin { get; set; }
     public DbSet<AspNetUserToken> UserToken { get; set; }
     public DbSet<AspNetRoleClaim> RoleClaim { get; set; }
+    public DbSet<Form> Forms { get; set; }
 }
