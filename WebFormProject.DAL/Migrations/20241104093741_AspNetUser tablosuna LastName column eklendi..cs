8fbbdab3810895fa88f729edf49bccbd29063e50
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2024-11-04 14:22:41 +0300

    migration dosyalari olusturuldu.

---
+++ WebFormProject.DAL/Migrations/20241104093741_AspNetUser tablosuna LastName column eklendi..cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebFormProject.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AspNetUsertablosunaLastNamecolumneklendi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+        }
+    }
+}
