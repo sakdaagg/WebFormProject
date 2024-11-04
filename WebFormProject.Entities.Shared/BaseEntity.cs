d3bc5c3aedabd21e459e5ad235fb209edeebca60
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2024-11-04 22:07:06 +0300

    Id vb ortak propertyleri tek bir model de toplamak icin BaseEntity class eklendi.

---
+++ WebFormProject.Entities.Shared/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace WebFormProject.Entities.Shared
+{
+    // This can be modified to BaseEntity<TId> to support multiple key types (e.g. Guid)
+    public abstract class BaseEntity<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+}
