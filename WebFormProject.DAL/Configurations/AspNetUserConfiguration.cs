5d2ace14d94948c8d54d9989533d750e4857b6e9
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2024-11-04 21:57:15 +0300

    AspNetUser tablosu ve Form tablolarina ait Configuration classlar olusturuldu.

---
+++ WebFormProject.DAL/Configurations/AspNetUserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebFromProject.Entities.Entities;
+
+namespace WebFormProject.DAL.Configurations;
+
+public class AspNetUserConfiguration : IEntityTypeConfiguration<AspNetUser>
+{
+    public void Configure(EntityTypeBuilder<AspNetUser> builder)
+    {
+
+        builder.HasKey(k => k.Id);
+
+        builder.HasMany(k => k.Forms)
+            .WithOne(k => k.CreatedBy).HasForeignKey(k => k.CreatedById)
+            .HasPrincipalKey(k => k.Id).OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ WebFormProject.DAL/Configurations/FormConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebFromProject.Entities.Entities;
+
+namespace WebFormProject.DAL.Configurations;
+
+public class FormConfiguration : IEntityTypeConfiguration<Form>
+{
+    public void Configure(EntityTypeBuilder<Form> builder)
+    {
+        builder.HasKey(k => k.Id);
+        builder.Property(k => k.Name).IsRequired().HasMaxLength(200);
+
+        builder.HasMany(k => k.Fields)
+           .WithOne(k => k.Form).HasForeignKey(k => k.FormId)
+           .HasPrincipalKey(k => k.Id).OnDelete(DeleteBehavior.Cascade);
+    }
+}
