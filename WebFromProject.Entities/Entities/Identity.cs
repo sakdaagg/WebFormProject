3cb18b4896cbfa7d3e4a0abcd807497f145ce7b4
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2024-11-04 14:21:03 +0300

    User sinifi, asp net core Identity den tureyecek sekilde tanimlandi.

---
+++ WebFromProject.Entities/Entities/Identity.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebFromProject.Entities.Entities
+{
+    public class AspNetUser : IdentityUser<Guid>
+    {
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+        [MaxLength(100)]
+        public string LastName { get; set; }
+    }
+    public class AspNetRole : IdentityRole<Guid>
+    {
+        public string Description { get; set; }
+    }
+    public class AspNetUserRole : IdentityUserRole<Guid>
+    {
+    }
+    public class AspNetUserClaim : IdentityUserClaim<Guid>
+    {
+    }
+    public class AspNetUserLogin : IdentityUserLogin<Guid>
+    {
+    }
+    public class AspNetUserToken : IdentityUserToken<Guid>
+    {
+    }
+    public class AspNetRoleClaim : IdentityRoleClaim<Guid>
+    {
+    }
+}
