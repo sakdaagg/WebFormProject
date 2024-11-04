d5fe5ba6aa09993b048c08f96d52ebc9ddbf05a4
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2024-11-04 14:15:26 +0300

    Kullanici login islemleri ve yeni kullanici ekleme islemleri icin LoginViewModel ve RegisterViewModel olusturuldu, Login ve Register page ler tanimlandi.

---
+++ WebFormProject/Models/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebFormProject.Models;
+
+public class LoginViewModel
+{
+    [Required(ErrorMessage = "Kullanıcı adı gereklidir")]
+    public string Username { get; set; }
+
+    [Required(ErrorMessage = "Şifre gereklidir")]
+    public string Password { get; set; }
+}
---
+++ WebFormProject/Models/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebFormProject.Models;
+
+public class RegisterViewModel
+{
+
+    [Required(ErrorMessage = "Ad gereklidir")]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Soyad gereklidir")]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "Kullanıcı adı gereklidir")]
+    public string Username { get; set; }
+
+    [Required(ErrorMessage = "Email gereklidir")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Şifre gereklidir")]
+    public string Password { get; set; }
+
+    [Compare("Password", ErrorMessage = "Şifreler eşleşmiyor")]
+    public string ConfirmPassword { get; set; }
+}
