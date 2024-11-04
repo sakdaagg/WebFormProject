87d382c67efbd9a1b88a1757c815040992624bfb
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2024-11-04 21:54:48 +0300

    Form islemleri icin gerekli repositoryler tanimlandi.

---
+++ WebFormProject.DAL/Abstract/IFormRepository.cs
@@ -0,0 +1,10 @@
+﻿using WebFromProject.Entities.Entities;
+
+namespace WebFormProject.DAL.Abstract;
+
+public interface IFormRepository
+{
+    Task<IEnumerable<Form>> GetAllFormsAsync(string search);
+    Task<Form> GetFormByIdAsync(int Id);
+    Task AddFormAsync(Form newForm);
+}
---
+++ WebFormProject.DAL/Concrete/FormRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebFormProject.DAL.Abstract;
+using WebFormProject.DAL.DbContexts;
+using WebFromProject.Entities.Entities;
+
+namespace WebFormProject.DAL.Concrete;
+
+public class FormRepository : IFormRepository
+{
+    private readonly WebFormDbContext _dbContext;
+
+    public FormRepository(WebFormDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task AddFormAsync(Form newForm)
+    {
+        await _dbContext.Forms.AddAsync(newForm);
+    }
+
+    public async Task<IEnumerable<Form>> GetAllFormsAsync(string search)
+    {
+        if (string.IsNullOrEmpty(search))
+            return await _dbContext.Forms.Include(k => k.Fields).ToListAsync();
+        else
+            return await _dbContext.Forms.Include(k => k.Fields).Where(k => k.Name.Equals(search)).ToListAsync();
+    }
+
+    public async Task<Form> GetFormByIdAsync(int Id)
+    {
+        if (Id == 0)
+            return null;
+        else
+            return await _dbContext.Forms.Include(k => k.Fields)
+                .FirstOrDefaultAsync(k => k.Id == Id);
+    }
+}
