c69999863cc5723e05a7012c61960f4f5c266e62
Selin UÇAR <bilg.muh.selin.ucar@gmail.com>
2024-11-04 22:01:50 +0300

    Form model tanimlandi.

---
+++ WebFromProject.Entities/Entities/Form.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebFormProject.Entities.Shared;
+
+namespace WebFromProject.Entities.Entities;
+
+public class Form : BaseEntity<int>
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime CreatedAt { get; set; }
+    public Guid CreatedById { get; set; }
+    public virtual AspNetUser CreatedBy { get; set; }
+    public ICollection<Field> Fields { get; set; } 
+}
+
+public class Field : BaseEntity<int>
+{
+    public bool Required { get; set; }
+    public string Name { get; set; }
+    public string DataType { get; set; }
+    public int FormId { get; set; }
+    public virtual Form Form { get; set; }
+}
